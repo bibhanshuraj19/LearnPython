fe805ead75d68d892ebb30cb59d8d8af01d0d3e8
RAJ <135547952+bibhanshuraj19@users.noreply.github.com>
2025-08-03 20:57:41 +0530

    update

--- strings.py
+++ strings.py
@@ -13,3 +13,13 @@ data = ("John", "Doe", 53.44)
 format_string = ("Hello %s %s. Your current balance is $%f" % (data))
 
 print(format_string)
+
+
+astring = "Hello world"
+astring2 = "Hello world!"
+print(astring,astring2)
+print(len(astring))
+print(astring.count("o"))
+print(astring.index("o"))
+
+print(astring[3:7:2])
