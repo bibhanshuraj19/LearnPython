c557b5e0aafed1da65bde9a243e35575d99bc6f8
RAJ <135547952+bibhanshuraj19@users.noreply.github.com>
2025-08-03 18:34:59 +0530

    update

--- operators.py
+++ operators.py
@@ -8,3 +8,23 @@ squared = 7 ** 2
 cubed = 2 ** 3
 print(squared)
 print(cubed)
+
+helloworld = "hello" + " " + "world"
+print(helloworld)
+
+hellos = "hello\n" * 10
+print(hellos)
+
+even = [2,4,6,8]
+odd = [1,3,5,7]
+all = odd + even
+print (all)
+
+print([1,3,4,5]*3)
+
+x = object()
+y = object()
+
+x_list = [x]*10
+y_list = [y]*10
+big_list = x_list + y_list
