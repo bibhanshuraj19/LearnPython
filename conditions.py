9000b22056c7b344dcd48fba1bc9ec34fc101003
RAJ <135547952+bibhanshuraj19@users.noreply.github.com>
2025-08-04 17:26:30 +0530

    update

--- conditions.py
+++ conditions.py
@@ -2,3 +2,35 @@ x = 1
 print(x == 1)
 print(x == 2)
 print(x < 3)
+
+name = "raj"
+age = 20
+if name == "raj" and age == 20:
+    print("ayoo")
+if name == "raj" or name == "virat":
+    print("ayoo2")
+
+name = "john"
+if name in ["john","Rick"]:
+    print("ayoo3")
+
+statement = False
+another_statement = True
+if statement is True:
+    pass
+elif another_statement is True:
+    pass
+else:
+    pass
+
+y = 2
+if y == 2 :
+    print("y equals 2")
+else :
+    print("y deosnot equals 2")
+
+x1 = [1,2,3]
+y1 = [1,2,3]
+print(x1 == y1) # True
+print(x1 is y1) # False
+
