274b2b92e644ba469bce50e7a5ebc2fdbd82917b
RAJ <135547952+bibhanshuraj19@users.noreply.github.com>
2025-08-05 00:10:43 +0530

    update

--- loops.py
+++ loops.py
@@ -16,3 +16,32 @@ count = 0
 while count < 5 :
     print(count)
     count += 1
+
+# This prints out 0,1,2,3,4
+count1 = 0
+while True :
+    print(count1)
+    count1 += 1
+    if count1 >= 5 :
+        break
+
+# This prints only odd numbers
+for x in range(10) :
+    if x % 2 == 0 :
+        continue
+    print(x)
+
+count2 = 0
+while(count2 < 5):
+    print(count2)
+    count2 += 1
+else :
+    print("Count have reached %d"%(count2))
+
+
+for i in range (1,10):
+    if i % 5 == 0:
+        break
+    print(i)
+else :
+    print("this is not a loop")
