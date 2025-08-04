b2fafd8074cd7237cf05e4c4b3ac32aa02a3acb2
RAJ <135547952+bibhanshuraj19@users.noreply.github.com>
2025-08-04 18:08:01 +0530

    update

---
+++ loops.py
@@ -0,0 +1,18 @@
+primes = [2,3,5,7]
+for prime in primes :
+    print(prime)
+
+for x in range(5) :
+    print(x)
+for x in range(3,8) :
+    print(x)
+
+range1 = []
+for x in range(3,8,2) :
+    range1.append(x)
+    print(range1)
+
+count = 0
+while count < 5 :
+    print(count)
+    count += 1
