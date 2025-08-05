7a83b9e162b5935349d019ca451473faa51e93a8
RAJ <135547952+bibhanshuraj19@users.noreply.github.com>
2025-08-05 13:55:35 +0530

    update

--- variables.py
+++ variables.py
@@ -10,3 +10,38 @@ x1 = "now this is a string"
 a = b = c = 100
 print(a,b,c)
 
+x, y, z = 1, 2.5, "hello"
+print(x,y,z)
+
+s = "10"
+n = int(s)
+cnt = 5
+f = float(cnt)
+age = 20
+s2 = str(age)
+print(type(age))
+print(type(s2))
+
+n1 = 42
+f1 = 3.14
+s = "Hello, World!"
+li = [1,2,3]
+d = {'key' : 'value'}
+bool =True
+
+print(type(bool))
+print(type(n1))
+print(type(f1))
+print(type(li))
+print(type(d))
+
+del d # this removes variable using del
+
+a1, b1 = 4, 10
+a1, b1 = b1, a1
+print(a1, b1)
+
+word = "Python"
+length = len(word)
+print(length)
+
