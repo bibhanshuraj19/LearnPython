918269c660c10bf1f44c6e088a34298ba539dfad
RAJ <135547952+bibhanshuraj19@users.noreply.github.com>
2025-08-10 16:31:38 +0530

    update

--- Advanced Python/main.py
+++ Advanced Python/main.py
@@ -6,3 +6,16 @@ print(item1_price)
 print(item1_quantity)
 print(item1_price_total)
 print()
+
+class Item() :
+    def __init__(self):
+        pass
+
+item2 = Item()
+item2.name = 'Phone'
+item2.price = 100
+print(item2.name)
+print(item2.price)
+
+random_str = 'aaa'
+print(random_str.upper())
